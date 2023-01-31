3cce028dbf2d5a3e72ab10ad6941da5dc37496a1
es6uid <es6uid@gmail.com>
2023-01-31 16:07:46 +0530

    discard

--- frontend-web/src/components/Videoplaceholder.js
+++ frontend-web/src/components/Videoplaceholder.js
@@ -53,8 +53,6 @@ const deleteSlot = async() => {
     })
 }
 
-// const iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/PuWouHWXIV4?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-
 const getMediaLink = () => {
     alert('hi')
 }

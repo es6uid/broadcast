1086011fe01fbb67bff214fb088d8381605fee55
es6uid <es6uid@gmail.com>
2023-02-02 16:26:17 +0530

    openai

--- frontend-web/src/components/Videoplaceholder.js
+++ frontend-web/src/components/Videoplaceholder.js
@@ -1,8 +1,16 @@
 import {useState, useEffect} from 'react';
-import axios from 'axios';
+import { Configuration, OpenAIApi } from 'openai';
+// import axios from 'axios';
+
+const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_API_KEY
+})
+const openai = new OpenAIApi(configuration)
 
 
 const Video = () => {
+    const [userImageInput, setUserImageInput] = useState("");
+    const [imageUrl, setImageUrl] = useState()
 
 const [testLink, setTestLink] = useState("9xaKQi9-VTI")
 const [iFrame, setIframe] = useState()
@@ -10,33 +18,51 @@ const [broadCast, setBroadCast] = useState()
 
 const [mediaLink, setMediaLink] = useState()
 
+const generateImage = async() => {
+
+    const imageParameters = {
+        prompt: userImageInput,
+        n: 1,
+        size: '256x256'
+    }
+    const response = await openai.createImage(imageParameters);
+    const urlData = response.data.data[0].url;
+    setImageUrl(urlData);
+}
+
 useEffect(() => {
     getVideo()
 },[])
 
 
 const getVideo = async () => {
-    const data = await fetch('http://localhost:5000/api/media');
+    // const data = await fetch('http://localhost:5000/api/media');
+    const data = await fetch('https://glorious-worm-poncho.cyclic.app/api/media');
+
     const json = await data.json()
     // setMediaLink
     setMediaLink(json[0])
     console.log(json[0])
 }
 
-const postVideo = async (url = 'http://localhost:5000/api/media', data={'text': encodeURIComponent(iFrame)} ) => {
-    const response = await fetch(url, {
+// const postVideo = async (url = 'http://localhost:5000/api/media', data={'text': encodeURIComponent(iFrame)} ) => {
+const postVideo = async (data={'text': encodeURIComponent(iFrame.split('/')[3])} ) => {
+    console.log({'text': encodeURIComponent(iFrame.split('/')[3])})
+    const response = await fetch('https://glorious-worm-poncho.cyclic.app/api/media', {
+        // const response = await fetch('http://localhost:5000/api/media', {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify({'text': encodeURIComponent(iFrame.split('/')[3])})
     })
 }
 
 const putVideo = async (data={'text': encodeURIComponent(iFrame.split('/')[3])}) => {
     const response = await fetch(`http://localhost:5000/api/media/${mediaLink?._id}`, {
-        method: 'PUT',
+    // const response = await fetch(`https://glorious-worm-poncho.cyclic.app/api/media/${mediaLink?._id}`, {
+            method: 'PUT',
         headers: {
         'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded'
@@ -48,31 +74,50 @@ const putVideo = async (data={'text': encodeURIComponent(iFrame.split('/')[3])})
 // const deleteSlot = async(url = `http://localhost:5000/api/media/${mediaLink?._id}`) => {
 const deleteSlot = async() => {
     console.log(`${mediaLink?._id}`)
-    const response = await fetch(`http://localhost:5000/api/media/${mediaLink?._id}`, {
-        method: 'DELETE',
+    // const response = await fetch(`http://localhost:5000/api/media/${mediaLink?._id}`, {
+    const response = await fetch(`https://glorious-worm-poncho.cyclic.app/api/media/${mediaLink?._id}`, {
+            method: 'DELETE',
     })
+    setMediaLink([])
 }
 
 const getMediaLink = () => {
     alert('hi')
 }
 
+const testEnv = () => {
+    console.log(process.env.REACT_APP_API_KEY)
+
+}
  return(
      <div>
+        <button onClick={testEnv}>Test Env</button>
+        <div>
+            {imageUrl && <img src={imageUrl} className='aiimage' alt='aiImage' />}
+            <label>Search</label>
+            <input onChange={(e) => setUserImageInput(e.target.value)}/ >
+                <button onClick = {generateImage}>Generate</button>
+        </div>
         <div>Video URL</div>
         {!mediaLink?.text ? 
             <div>
                 <input onInput={e => setIframe(e.target.value)}/> 
-                <button onClick = {() => putVideo()}>Broadcast</button>
+                <button onClick = {postVideo}>Broadcast</button>
             </div>
         : null}
         {/* <div dangerouslySetInnerHTML={ {__html:  mediaLink?decodeURIComponent(mediaLink.text):""}} /> */}
+        {mediaLink?.text?
         <iframe className="ytplayer" width="560" height="315" src={`https://www.youtube-nocookie.com/embed/${mediaLink?.text}?controls=0`}
         title="YouTube video player123" frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         allowFullScreen></iframe>
+        :null}
 
-        <div onClick={deleteSlot}>Delete Slot</div>
+        <button onClick={deleteSlot}>Delete Slot</button>
+        {!mediaLink?.text ?
+            <button onClick={getVideo}>New session</button>
+            :null
+        }
 
         {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/bV270I2hXpQ?autoplay=1&mute=1&showinfo=0&controls=1&modestbranding=0&autohide=0" 
         title="YouTube video player" frameBorder="0" 

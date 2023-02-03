acbe9fd993535377ae23f711afba0f5cfc250a4c
es6uid <es6uid@gmail.com>
2023-02-03 20:18:03 +0530

    Added AI and wrapped with Bootstrap

--- frontend-web/src/components/Videoplaceholder.js
+++ frontend-web/src/components/Videoplaceholder.js
@@ -1,134 +1,141 @@
-import {useState, useEffect} from 'react';
-import { Configuration, OpenAIApi } from 'openai';
-// import axios from 'axios';
+import { useState, useEffect } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import CreateClass from './Createclass';
 
 const configuration = new Configuration({
-    apiKey: process.env.REACT_APP_API_KEY
-})
-const openai = new OpenAIApi(configuration)
+  apiKey: process.env.REACT_APP_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
+// const api = `https://glorious-worm-poncho.cyclic.app/api/media`
+const api = `http://localhost:5000/api/media/`
 
 const Video = () => {
-    const [userImageInput, setUserImageInput] = useState("");
-    const [imageUrl, setImageUrl] = useState()
+  const [userImageInput, setUserImageInput] = useState("");
+  const [description, setDescription] = useState("");
+  const [thumbnail, setThumbnail] = useState();
+  const [iFrame, setIframe] = useState();
+  const [mediaLink, setMediaLink] = useState();
+  const [visibility, setVisibility] = useState('hello');
 
-const [testLink, setTestLink] = useState("9xaKQi9-VTI")
-const [iFrame, setIframe] = useState()
-const [broadCast, setBroadCast] = useState()
 
-const [mediaLink, setMediaLink] = useState()
-
-const generateImage = async() => {
-
+  const generateImage = async () => {
     const imageParameters = {
-        prompt: userImageInput,
-        n: 1,
-        size: '256x256'
-    }
+      prompt: userImageInput,
+      n: 1,
+      size: "256x256",
+    };
     const response = await openai.createImage(imageParameters);
     const urlData = response.data.data[0].url;
-    setImageUrl(urlData);
-}
+    setThumbnail(urlData);
+  };
 
-useEffect(() => {
-    getVideo()
-},[])
+  const clearAIImage = () => setThumbnail('');
 
+  useEffect(() => {
+    getVideo();
+  }, []);
 
-const getVideo = async () => {
-    // const data = await fetch('http://localhost:5000/api/media');
-    const data = await fetch('https://glorious-worm-poncho.cyclic.app/api/media');
+  const getVideo = async () => {
+    const data = await fetch(api);
+    const json = await data.json();
+    setMediaLink(json[0]);
+    // console.log(json[0]);
+  };
 
-    const json = await data.json()
-    // setMediaLink
-    setMediaLink(json[0])
-    console.log(json[0])
-}
-
-// const postVideo = async (url = 'http://localhost:5000/api/media', data={'text': encodeURIComponent(iFrame)} ) => {
-const postVideo = async (data={'text': encodeURIComponent(iFrame.split('/')[3])} ) => {
-    console.log({'text': encodeURIComponent(iFrame.split('/')[3])})
-    const response = await fetch('https://glorious-worm-poncho.cyclic.app/api/media', {
-        // const response = await fetch('http://localhost:5000/api/media', {
-        method: 'POST',
+  const postVideo = async () => {
+    // console.log(iFrame, thumbnail )
+    await fetch(api, {
+        method: "POST",
         headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({'text': encodeURIComponent(iFrame.split('/')[3])})
-    })
-}
+        body: JSON.stringify({
+          text: encodeURIComponent(iFrame.split("/")[3]),
+          thumbnail,
+          description
+        }),
+      }
+    );
+  };
 
-const putVideo = async (data={'text': encodeURIComponent(iFrame.split('/')[3])}) => {
-    const response = await fetch(`http://localhost:5000/api/media/${mediaLink?._id}`, {
-    // const response = await fetch(`https://glorious-worm-poncho.cyclic.app/api/media/${mediaLink?._id}`, {
-            method: 'PUT',
-        headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: JSON.stringify(data)
-    })
-}
+//   const putVideo = async (
+//     data = { text: encodeURIComponent(iFrame.split("/")[3]) }
+//   ) => {
+//     const response = await fetch(
+//       `http://localhost:5000/api/media/${mediaLink?._id}`,
+//       {
+//         // const response = await fetch(`https://glorious-worm-poncho.cyclic.app/api/media/${mediaLink?._id}`, {
+//         method: "PUT",
+//         headers: {
+//           "Content-Type": "application/json",
+//           // 'Content-Type': 'application/x-www-form-urlencoded'
+//         },
+//         body: JSON.stringify(data),
+//       }
+//     );
+//   };
 
-// const deleteSlot = async(url = `http://localhost:5000/api/media/${mediaLink?._id}`) => {
-const deleteSlot = async() => {
-    console.log(`${mediaLink?._id}`)
-    // const response = await fetch(`http://localhost:5000/api/media/${mediaLink?._id}`, {
-    const response = await fetch(`https://glorious-worm-poncho.cyclic.app/api/media/${mediaLink?._id}`, {
-            method: 'DELETE',
-    })
-    setMediaLink([])
-}
+  // const deleteSlot = async(url = `http://localhost:5000/api/media/${mediaLink?._id}`) => {
+  const deleteSlot = async () => {
+    console.log(`${mediaLink?._id}`);
+    await fetch(`${api}/${mediaLink?._id}`,{
+        method: "DELETE",
+      }
+    );
+    setMediaLink([]);
+  };
 
-const getMediaLink = () => {
-    alert('hi')
-}
+  return (
+    <div>
+        <CreateClass 
+        thumbnail={thumbnail} 
+        setUserImageInput={setUserImageInput} 
+        generateImage={generateImage}
+        mediaLink={mediaLink}
+        setIframe={setIframe}
+        setDescription={setDescription}
+        postVideo={postVideo}
+        clearAIImage={clearAIImage}
+    />
 
-const testEnv = () => {
-    console.log(process.env.REACT_APP_API_KEY)
 
-}
- return(
-     <div>
-        <button onClick={testEnv}>Test Env</button>
-        <div>
-            {imageUrl && <img src={imageUrl} className='aiimage' alt='aiImage' />}
-            <label>Search</label>
-            <input onChange={(e) => setUserImageInput(e.target.value)}/ >
-                <button onClick = {generateImage}>Generate</button>
-        </div>
-        <div>Video URL</div>
-        {!mediaLink?.text ? 
-            <div>
-                <input onInput={e => setIframe(e.target.value)}/> 
-                <button onClick = {postVideo}>Broadcast</button>
+<div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-2">
+                        {mediaLink?.thumbnail && <img src={mediaLink.thumbnail} className="m-2 img-thumbnail rounded" alt="aiImage" width='200' height='200' />}
+                    </div>
+                    <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title">Create a class</h5>
+                        <p class="card-text">{mediaLink?.description && <p>{mediaLink.description}</p>}</p>
+                        <p class="card-text"><small class="text-muted">Created on updated 3 mins ago, created by: Nupur</small></p>
+                        <button onClick={deleteSlot} type="button" className="btn btn-outline-danger btn-sm me-2" data-bs-dismiss="modal">Delete</button>
+                        <button onClick={deleteSlot} type="button" className="btn btn-outline-success btn-sm" data-bs-dismiss="modal">Play</button>
+                    </div>
+                    </div>
+                </div>
             </div>
-        : null}
-        {/* <div dangerouslySetInnerHTML={ {__html:  mediaLink?decodeURIComponent(mediaLink.text):""}} /> */}
-        {mediaLink?.text?
-        <iframe className="ytplayer" width="560" height="315" src={`https://www.youtube-nocookie.com/embed/${mediaLink?.text}?controls=0`}
-        title="YouTube video player123" frameBorder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        allowFullScreen></iframe>
-        :null}
+      {mediaLink?.text ? (
+        <iframe
+          className="ytplayer"
+          width="560"
+          height="315"
+          src={`https://www.youtube-nocookie.com/embed/${mediaLink?.text}?controls=0`}
+          title="YouTube video player123"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      ) : null}
 
-        <button onClick={deleteSlot}>Delete Slot</button>
-        {!mediaLink?.text ?
-            <button onClick={getVideo}>New session</button>
-            :null
-        }
-
-        {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/bV270I2hXpQ?autoplay=1&mute=1&showinfo=0&controls=1&modestbranding=0&autohide=0" 
-        title="YouTube video player" frameBorder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        allowFullScreen></iframe> */}
-     </div>
- )   
-}
-export default Video
+      {/* <button onClick={deleteSlot}>Delete Slot</button>
+      {!mediaLink?.text ? (
+        <button onClick={getVideo}>New session</button>
+      ) : null} */}
+    </div>
+  );
+};
+export default Video;
 
 // https://youtu.be/9xaKQi9-VTI
-// https://www.youtube.com/watch?v=9xaKQi9-VTI&ab_channel=codedamn
-// {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/9xaKQi9-VTI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-        // <iframe width="560" height="315" src="https://www.youtube.com/embed/PuWouHWXIV4?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
